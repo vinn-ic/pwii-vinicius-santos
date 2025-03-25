2c390573d6a8558896b01a54a71a112872be95c4
vinicius angelus <viniciusangelus30@gmail.com>
2025-03-25 16:27:18 -0300

    aula 02

---
+++ aula02/concatenar.php
@@ -0,0 +1,15 @@
+<?php
+echo "hello world!<br>";
+echo "hello world!<br>";
+Echo "hello world!<br>";
+
+$nome = "vinicius";
+$sobrenome = "angelus";
+$numero = 4;
+
+echo $nome . " lind" . "<br>";
+echo "${nome} <h1> ${sobrenome}<h1>";
+echo $numero . "<p>" . $nome . 
+" " .  $sobrenome . "</p>";
+
+?>
